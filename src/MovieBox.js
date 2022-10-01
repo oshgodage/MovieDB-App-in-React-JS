ed9527ccdc694b86900684a364d37e3efd752511
Ebukadev <degwu2020@gmail.com>
2022-10-01 01:12:56 +0100

    Corrected the destructured parameter of the movieReq response object

--- src/MovieBox.js
+++ src/MovieBox.js
@@ -2,7 +2,7 @@ import { Modal,show,Button} from 'react-bootstrap';
 import React, {useState} from 'react';
 const API_IMG="https://image.tmdb.org/t/p/w500/";
 
-const MovieBox =({titel, poster_path, vote_average, release_date, overview})=>{
+const MovieBox =({title, poster_path, vote_average, release_date, overview})=>{
     
     const [show, setShow]=useState(false);
 
@@ -21,7 +21,7 @@ const MovieBox =({titel, poster_path, vote_average, release_date, overview})=>{
                       </Modal.Header>
                       <Modal.Body>
                       <img className="card-img-top" style={{width:'14rem'}}src={API_IMG+poster_path} />
-                      <h3>{titel}</h3>
+                      <h3>{title}</h3>
                       <h4>IMDb: {vote_average}</h4>
                       <h5>Release Date: {release_date}</h5>
                       <br></br>
